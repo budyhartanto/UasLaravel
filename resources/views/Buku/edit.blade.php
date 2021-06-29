84ca4813b5ab04bb1594acab8755eb0f77a2dc8d
Hartanto <50822859+budyhartanto@users.noreply.github.com>
2021-06-29 10:49:12 +0700

    Update edit.blade.php

--- resources/views/Buku/edit.blade.php
+++ resources/views/Buku/edit.blade.php
@@ -11,7 +11,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-  <h4 class="text-center">Form Edit Data Buku</h4>
+  <h4 class="text-center">Form Edit Data Barang</h4>
   
     <form action="{{route('buku.update',$buku->id)}} " method="POST" enctype="multipart/form-data">
     @csrf
